5285056a8c26028ad63beb5f2b7283728b04fc09
Danilo <danilo.2500@gmail.com>
2026-05-14 17:39:55 -0300

    re add sleep to open splash screen

---
+++ iTunesChallenge/WatchItunesChallenge Watch App/Screens/SongsView/SongsView.swift
@@ -0,0 +1,18 @@
+//
+//  SongsView.swift
+//  WatchItunesChallenge Watch App
+//
+//  Created by Danilo Henrique on 14/05/26.
+//
+
+import SwiftUI
+
+struct SongsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongsView()
+}
---
+++ iTunesChallenge/WatchItunesChallenge Watch App/WatchItunesChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchItunesChallengeApp.swift
+//  WatchItunesChallenge Watch App
+//
+//  Created by Danilo Henrique on 14/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchItunesChallenge_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
